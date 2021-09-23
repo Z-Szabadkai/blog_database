39867d71c7bd7d8940c68e90b98f94897bb1cbf4
Z-Szabadkai <zoltan.szabadkai@gmail.com>
2021-09-23 22:09:29 +0200

    Classes added to the project

---
+++ src/main/java/model/Privilege.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum Privilege {
+
+    USER,
+    MODERATOR,
+    ADMIN;
+
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,10 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+public class User {
+
+}
