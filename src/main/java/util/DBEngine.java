f20fe4b4d1c6f12e53bf020ebb5868d49e043948
Z-Szabadkai <zoltan.szabadkai@gmail.com>
2021-09-26 21:41:33 +0200

    Added DBEngine class, and connectToDB method

---
+++ src/main/java/util/DBEngine.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBEngine {
+
+    private Connection connecToDB() {
+        String url = "jdbc:mysql://localhost:3306/blogDB" +
+                "?useUnicode=yes&characterEncoding=UTF-8";
+
+        Properties properties = new Properties();
+        properties.put("user", System.getenv("DB_USER"));
+        properties.put("password", System.getenv("DB_PASSWORD"));
+
+        try {
+            return DriverManager.getConnection(url, properties);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
